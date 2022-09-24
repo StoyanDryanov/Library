632e00772224a428c19a369107ae2ce90f49f609
Stoyan <stoyandryanov@gmail.com>
2022-09-24 20:04:03 +0300

    Added functionality

--- app.js
+++ app.js
@@ -1,12 +1,99 @@
-let library = [];
+let Library = [];
 
-function Book(author, title, pages, read) {
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.read = read
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function addBookToLibrary () {
-    
+const submitBtn = document.querySelector('[type=submit]');
+
+submitBtn.addEventListener('click', addBookToLibrary);
+
+function render() {
+  let display = document.querySelector(".library");
+  display.innerHTML = '';
+
+  for (let i = 0; i < Library.length; i++) {
+    createBook(Library[i]);
+  }
+}
+
+function addBookToLibrary() {
+    event.preventDefault();
+
+    let title = document.getElementById('Title').value;
+    let author = document.getElementById('Author').value;
+    let pages = document.getElementById('Pages').value;
+    let read = document.getElementById('is-read').checked;
+
+    let newBook = new Book(title, author, pages, read);
+
+    for (const prop in newBook) {
+        if (newBook[prop] === '') {
+            return;
+        }
+    } // if any of the field are empty then nothing will be added
+
+    Library.push(newBook);
+
+    render();
+
+    form.reset();
+}
+
+function createBook(book) {
+
+  let libraryContainer = document.querySelector(".library");
+  let bookDiv = document.createElement("div");
+  let title = document.createElement("p");
+  let author = document.createElement("p");
+  let pages = document.createElement("p");
+  let isReadBtn = document.createElement("button");
+  let deleteBtn = document.createElement("button");
+
+  bookDiv.classList.add("book");
+
+  if (book.read) {
+    isReadBtn.classList.add("read");
+  } else {
+    isReadBtn.classList.add("not-read");
+  }
+
+  deleteBtn.classList.add("delete");
+
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+  isReadBtn.innerText = book.read ? "Read" : "Not Read";
+  deleteBtn.innerText = "Delete";
+
+  isReadBtn.addEventListener("click", () => {
+    book.read = !book.read;
+    render();
+  });
+
+  deleteBtn.addEventListener('click', () => {
+    Library.splice(Library.indexOf(book), 1);
+    render();
+  })
+
+  bookDiv.appendChild(title);
+  bookDiv.appendChild(author);
+  bookDiv.appendChild(pages);
+  bookDiv.appendChild(isReadBtn);
+  bookDiv.appendChild(deleteBtn);
+
+  libraryContainer.appendChild(bookDiv);
+}
+
+function isNotFull(book) {
+    for (const prop in book) {
+        if (book[prop] === '') {
+            return;
+        }
+    }
 }
